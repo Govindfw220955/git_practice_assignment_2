f6f5397a29337be52f52960e57987660aff98c5c
Govind Saini <kinggovind88@gmail.com>
2022-10-21 10:00:13 +0530

    i have created two file of Prime and Palindroma Problem

---
+++ Prime_Problem.js
@@ -0,0 +1,13 @@
+//Problem 2: Print Prime Numbers from 1 to given limit
+
+let num=17;
+let count=0;
+
+for(let i=0; i<=num; i++){
+        count++;
+    }
+if(count==2){
+    console.log("Yes");
+} else{
+    console.log("No");
+}
